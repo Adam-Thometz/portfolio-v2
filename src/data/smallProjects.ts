70a4cd0ead368fb78646d2478438eea2548940e3
Adam Thometz <adamthometz@gmail.com>
2023-09-22 12:01:39 -0400

    updated projects

--- src/data/smallProjects.ts
+++ src/data/smallProjects.ts
@@ -3,7 +3,8 @@ import { Project } from "./types";
 
 import MUSIC_RAINBOW from "../../public/gifs/music-rainbow.gif";
 import EIGHT_BIT_CLAN from '../../public/gifs/8-bit-clan.gif';
-import ACCEPTANCE_CAVE from "../../public/gifs/acceptance-cave.gif"
+import ACCEPTANCE_CAVE from "../../public/gifs/acceptance-cave.gif";
+import PORTFOLIO from "../../public/gifs/portfolio.gif";
 
 const smallProjects: Project[] = [
   {
@@ -35,12 +36,21 @@ const smallProjects: Project[] = [
   // },
   {
     name: "8-Bit Clan",
-    description: "An app that makes 8-bit NFTs. Placed in the top 5% in the Mintbean Hiring Hackathon.",
+    description: "Make some 8-bit NFTs! Top 5% in the Mintbean Hiring Hackathon",
     techStack: getTechStack(["Node", "Express", "React", "PostgresQL"]),
     gif: EIGHT_BIT_CLAN,
     liveLink: new URL("https://8-bit-clan.surge.sh/"),
     githubLink: new URL("https://github.com/Adam-Thometz/8-Bit-Clan"),
     fakeUrl: "8-bit.clan"
+  },
+  {
+    name: "Portfolio",
+    description: "I mean, this portfolio was a labor of love after all.",
+    techStack: getTechStack(["NextJS", "Typescript"]),
+    gif: PORTFOLIO,
+    liveLink: new URL("https://www.adamthometz.com/"),
+    githubLink: new URL("https://github.com/Adam-Thometz/portfolio-v2"),
+    fakeUrl: "adam.thometz"
   },
 ];
 
