ff54f7f5df9578f9d3fa277ee6158d4365fda2f8
Adam Thometz <adamthometz@gmail.com>
2023-09-28 15:09:47 -0400

    updated resume

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -3,4 +3,4 @@ export const PROJECTS = "projects";
 export const PROJECT = "project";
 export const ABOUT_ME = "aboutMe";
 export const PLAYGROUND = "playground";
-export const RESUME_LINK = "https://drive.google.com/file/d/1Za_Gt3pLPblCCN9qUQfH8HzVjmt6El6S/view?usp=sharing";
+export const RESUME_LINK = "https://drive.google.com/file/d/1wsVY8D2Nxr8Pus11mrfDnk8OZ2ATqElb/view?usp=sharing";
