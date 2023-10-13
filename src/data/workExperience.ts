90d836c663a8628eaf877fd5e2c846ad65a9c72b
Adam Thometz <adamthometz@gmail.com>
2023-10-13 17:00:22 -0400

    reorganized public folder

--- src/data/workExperience.ts
+++ src/data/workExperience.ts
@@ -7,7 +7,7 @@ const projects: WorkExperience[] = [
     description: "GPT-powered tools to help teachers create goals, quizzes, and tasks while saving hours of planning time.",
     techStack: getTechStack(["Vue", "Django", "AWS", "Node"]),
     mp4: "mp4s/ai-assistant.mp4",
-    fallback: "mp4s/fallback/ai-assistant.png",
+    fallback: "images/fallbacks/ai-assistant.png",
     liveLink: new URL("https://www.tarateachers.com/"),
     fakeUrl: "tara.teachers"
   },
@@ -16,7 +16,7 @@ const projects: WorkExperience[] = [
     description: "A website to advertise the services of a local psychotherapist.",
     techStack: getTechStack(["React", "CSS", "AWS"]),
     mp4: "mp4s/clarity.mp4",
-    fallback: "mp4s/fallback/vesta.png",
+    fallback: "images/fallbacks/vesta.png",
     liveLink: new URL("https://www.claritypsychotherapycounseling.com/"),
     fakeUrl: "clarity.healing"
   },
@@ -25,7 +25,7 @@ const projects: WorkExperience[] = [
     description: "Arts-based and goal-aligned games and tools for the special education classroom.",
     techStack: getTechStack(["React", "Redux", "CSS", "Jest"]),
     mp4: "mp4s/art-buddy.mp4",
-    fallback: "mp4s/fallback/art-buddy.png",
+    fallback: "images/fallbacks/art-buddy.png",
     liveLink: new URL("https://art-buddy.surge.sh/"),
     fakeUrl: "art.buddy"
   },
