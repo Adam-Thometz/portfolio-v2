3174cadafcbd1293d098109caa4d67060a1bc89f
Adam Thometz <adamthometz@gmail.com>
2023-10-13 16:45:50 -0400

    changed vesta assets, changed description of 8bm, and fixed potential bug with fallback

--- src/data/workExperience.ts
+++ src/data/workExperience.ts
@@ -7,36 +7,36 @@ const projects: WorkExperience[] = [
     description: "GPT-powered tools to help teachers create goals, quizzes, and tasks while saving hours of planning time.",
     techStack: getTechStack(["Vue", "Django", "AWS", "Node"]),
     mp4: "mp4s/ai-assistant.mp4",
-    fallback: "mp4s/fallback/ai-assistant.mp4",
+    fallback: "mp4s/fallback/ai-assistant.png",
     liveLink: new URL("https://www.tarateachers.com/"),
     fakeUrl: "tara.teachers"
-  },
-  {
-    name: "8billionminds - Frontend",
-    description: "Like couch-surfing but for live learning. Learn from and connect with others from around the world.",
-    techStack: getTechStack(["Javascript", "CSS", "Java"]),
-    mp4: "mp4s/8bm.mp4",
-    fallback: "mp4s/fallback/8bm.mp4",
-    liveLink: new URL("https://8billionminds.com/"),
-    fakeUrl: "8bm.live"
   },
   {
     name: "Clarity - Frontend",
     description: "A website to advertise the services of a local psychotherapist.",
     techStack: getTechStack(["React", "CSS", "AWS"]),
-    mp4: "mp4s/vesta.mp4",
-    fallback: "mp4s/fallback/vesta.mp4",
+    mp4: "mp4s/clarity.mp4",
+    fallback: "mp4s/fallback/vesta.png",
     liveLink: new URL("https://www.claritypsychotherapycounseling.com/"),
-    fakeUrl: "vesta.healing"
+    fakeUrl: "clarity.healing"
   },
   {
     name: "Art Buddy - Frontend",
     description: "Arts-based and goal-aligned games and tools for the special education classroom.",
     techStack: getTechStack(["React", "Redux", "CSS", "Jest"]),
     mp4: "mp4s/art-buddy.mp4",
-    fallback: "mp4s/fallback/art-buddy.mp4",
+    fallback: "mp4s/fallback/art-buddy.png",
     liveLink: new URL("https://art-buddy.surge.sh/"),
     fakeUrl: "art.buddy"
+  },
+  {
+    name: "8billionminds - Frontend",
+    description: "Like couch-surfing but for live learning. I volunteer on this project from time to time.",
+    techStack: getTechStack(["Javascript", "CSS", "Java"]),
+    mp4: "mp4s/8bm.mp4",
+    fallback: "mp4s/fallback/8bm.png",
+    liveLink: new URL("https://8billionminds.com/"),
+    fakeUrl: "8bm.live"
   },
 ];
 
