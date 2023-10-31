857236111c71d032d53311b1800b9c167e3cba00
Adam Thometz <adamthometz@gmail.com>
2023-10-31 15:01:31 -0400

    changed order

--- src/data/workExperience.ts
+++ src/data/workExperience.ts
@@ -3,6 +3,15 @@ import { WorkExperience } from "./types";
 
 const projects: WorkExperience[] = [
   {
+    name: "Art Buddy - Frontend",
+    description: "Arts-based and goal-aligned games and tools for the special education classroom.",
+    techStack: getTechStack(["React", "Redux", "CSS", "Jest"]),
+    mp4: "mp4s/art-buddy.mp4",
+    fallback: "images/fallbacks/art-buddy.webp",
+    liveLink: new URL("https://art-buddy.surge.sh/"),
+    fakeUrl: "art.buddy"
+  },
+  {
     name: "TARA - AI Assistant",
     description: "GPT-powered tools to help teachers create goals, quizzes, and tasks while saving hours of planning time.",
     techStack: getTechStack(["Vue", "Django", "AWS", "Node"]),
@@ -19,15 +28,6 @@ const projects: WorkExperience[] = [
     fallback: "images/fallbacks/vesta.webp",
     liveLink: new URL("https://www.claritypsychotherapycounseling.com/"),
     fakeUrl: "clarity.healing"
-  },
-  {
-    name: "Art Buddy - Frontend",
-    description: "Arts-based and goal-aligned games and tools for the special education classroom.",
-    techStack: getTechStack(["React", "Redux", "CSS", "Jest"]),
-    mp4: "mp4s/art-buddy.mp4",
-    fallback: "images/fallbacks/art-buddy.webp",
-    liveLink: new URL("https://art-buddy.surge.sh/"),
-    fakeUrl: "art.buddy"
   },
 ];
 
